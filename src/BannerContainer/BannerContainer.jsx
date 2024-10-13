f515d1bd8d708fc982b99c5e5cb17d83fa3b19dc
psaxone <saxonejazz@gmail.com>
2024-10-13 20:52:16 +0200

    new banner image and responsive layout

--- src/BannerContainer/BannerContainer.jsx
+++ src/BannerContainer/BannerContainer.jsx
@@ -1,44 +1,33 @@
 import { Box, Typography } from '@mui/material'
-import Author from '../../src/assets/autorb&w.png'
+import Author from '../../src/assets/BannerB&W.png'
 
 
 function BannerContainer() {
   const styles = {
-    boxContainer: {
-      display: 'flex',
-      justifyContent: 'space-between'
-    },
-    textBackground: {
-      display: 'flex',
-      backgroundColor: 'black',
-      width: '60%'
-    },
     textContainer: {
       color: 'white',
       alignItems: 'center',
       display: 'flex',
       marginLeft: '32px'
     },
-    imageContainer: {
-      // width: '40%'
+    boxContainer: {
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      padding: '250px 0px',
+      backgroundImage: `url(${Author})`,
+      backgroundAttachment: 'scroll',
+      backgroundPosition: 'center'
     }
   }
   return (
-    <Box sx={styles.boxContainer}>
-      <Box sx={styles.textBackground}>
-        <Box sx={styles.textContainer}>
-          <Typography
-            variant='h2'
-            align='center'
-          >
-            Alejandro Cardoso
-          </Typography>
-        </Box>
+  <Box sx={styles.boxContainer}>
+      <Box sx={styles.textContainer}>
+        <Typography
+          variant='h2'
+        >
+          Alejandro Cardoso
+        </Typography>
       </Box>
-      <Box sx={styles.imageContainer}>
-        <img src={Author} alt='autor'/>
-      </Box>
-      
     </Box>
   );
 }
--- src/BookDescriptionContainer/BookDescriptionContainer.jsx
+++ src/BookDescriptionContainer/BookDescriptionContainer.jsx
@@ -1,8 +1,14 @@
 import { Box } from '@mui/material'
 import ProductCard from './ProductCard'
-import BookDescription from './BookDescription';
+import BookDescription from './BookDescription'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
 
 function BookDescriptionContainer() {
+
+  const theme = useTheme();
+  const isMd = useMediaQuery(theme.breakpoints.up('md'));
+
   const styles = {
     bookDescriptionContainer: {
       display: 'flex',
@@ -10,11 +16,18 @@ function BookDescriptionContainer() {
       marginRight: 'auto',
       marginLeft: 'auto',
       padding: '150px 80px'
+    },
+    bookDescriptionContainerSmall: {
+      display: 'flex',
+      maxWidth: '1800px',
+      flexDirection: 'column',
+      margin: '64px 16px',
+      alignItems: 'center'
     }
   }
 
   return (
-    <Box sx={styles.bookDescriptionContainer}>
+    <Box sx={isMd ? styles.bookDescriptionContainer : styles.bookDescriptionContainerSmall }>
       <ProductCard />
       <BookDescription />
     </Box>
