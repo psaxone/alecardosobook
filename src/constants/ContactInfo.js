5e9825c23428042bacd817f80d69f5ba3403125a
psaxone <saxonejazz@gmail.com>
2024-10-15 15:57:33 +0200

    modified book description

--- src/constants/ContactInfo.js
+++ src/constants/ContactInfo.js
@@ -1,6 +1,6 @@
 const ContactInfoConstant = {
   phoneNumber: process.env.REACT_APP_PUBLIC_PHONE_NUMBER,
-  whatsappContactText: 'Hola! quisiera comprar tu libro con la siguiente dedicatoria: En el oeste son todos trolos',
+  whatsappContactText: '¡Bienvenido! ¡Te damos las gracias por comunicarte con nosotros! Por favor, decinos como podemos ayudarte. En este momento no estamos disponibles, pero a la brevedad tendremos una respuesta para vos.',
   instagram: process.env.REACT_APP_PUBLIC_INSTAGRAM,
   facebook: process.env.REACT_APP_FACEBOOK,
   twitter: process.env.REACT_APP_TWITTER,
